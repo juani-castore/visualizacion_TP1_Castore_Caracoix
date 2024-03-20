1a5cbb11c6bd0a76b9b20d6355316381ac4246c5
felipe <felipecaracoix@gmail.com>
2024-03-20 11:08:01 -0300

    primera version

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<!-- Script JS -->
+<script>
+    var x = 67;
+    x = Math.round(x/10)
+    const images = []
+    for(let i =0;i<x;i++ ){
+        images.push("./images/podrida1.png")
+    }
+    for(let i=0;i<10-x;i++){
+        images.push("./images/zanahoria.webp")
+    }
+
+</script>
+
+<!-- Estructura contenido HTML -->
+<!-- cantidad de tiempo de vida de la zanahoria o x% zanahorias podridas d -->
+<input type="number" bind:value={x}>
+<div class="grid">
+    {#each images as image}
+      <img src={image} alt="Imagen">
+    {/each}
+  </div>
+
+<!-- Estilos CSS -->
+<style> 
+.grid {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(2, auto);
+}
+img{
+    height: 100px;
+    width: 100px;
+}
+</style>
